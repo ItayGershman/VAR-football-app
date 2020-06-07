99c487cac755fb6ac781408c6979e09b820d945d
AvielDagan <48187820+AvielDagan@users.noreply.github.com>
2020-06-07 20:55:34 +0300

    Added league name and teams through the api

--- src/comp/Livescore.js
+++ src/comp/Livescore.js
@@ -4,6 +4,8 @@ import { StyleSheet, View, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import Header from './Header';
 import DataContainerStyles from '../styles'
+import SvgUri from 'react-native-svg';
+
 
 // const data = fetch('https://api-football-v1.p.rapidapi.com/v2/fixtures/live/754', { //live games of the german league
 //   // fetch('https://api-football-v1.p.rapidapi.com/v2/leagues', { // all the leagues
@@ -32,36 +34,43 @@ class Livescore extends React.Component {
   constructor() {
     super()
     this.state = {
-      matchData: []
+      matchData: [],
+      matchHome: [],
+      matchAway: [],
+      leagueFlag: []
     };
   }
 
-  // componentDidMount() {
-  //   this._isMounted = true
-  //   fetch('https://api-football-v1.p.rapidapi.com/v2/fixtures/live/754',{
-  //     "method": "GET",
-  //     "headers": {
-  //     "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-  //     "x-rapidapi-key": "b78d8edbacmsh0d14864fbf5ad4ap1427d6jsn0b94b1b8d032"
-  //     }})
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log('Success:', data);
-  //       console.log(data.api.fixtures[0].league.name)
-  //       if (this._isMounted) {
-  //         this.setState({
-  //           matchData: data.api,
-  //         })
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.error(error)
-  //     })
-  // }
+  componentDidMount() {
+    this._isMounted = true
+    fetch('https://api-football-v1.p.rapidapi.com/v2/fixtures/live/754', {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+        "x-rapidapi-key": "b78d8edbacmsh0d14864fbf5ad4ap1427d6jsn0b94b1b8d032"
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+        console.log(data.api.fixtures[0].league.name)
+        if (this._isMounted) {
+          this.setState({
+            matchLeague: data.api.fixtures[0].league.name,
+            leagueFlag: data.api.fixtures[0].league.flag,
+            matchHome: data.api.fixtures[0].homeTeam,
+            matchAway: data.api.fixtures[0].awayTeam
+          })
+        }
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
 
-  // componentWillUnmount() {
-  //   this._isMounted = false
-  // }
+  componentWillUnmount() {
+    this._isMounted = false
+  }
 
   render() {
     return (
@@ -69,11 +78,17 @@ class Livescore extends React.Component {
         <Header navigation={this.props.navigation} />
         <View style={DataContainerStyles.dataContainer}>
           <Text style={styles.text}> Livescore</Text>
-          <Text>Leauge:</Text>
+          <View style={styles.leagueBox}>
+            <Text style={styles.leagueName}>{this.state.matchLeague}</Text>
+            {/* <SvgUri
+            style={styles.flag}
+              width="100"
+              height="100"
+              source={{ uri: this.state.leagueFlag }} /> */}
+            {/* <View><Image>{this.leagueFlag}</Image></View> */}
+            <Text style={styles.leagueName}>{this.state.matchHome.team_name} vs {this.state.matchAway.team_name}</Text>
+          </View>
         </View>
-        <View>
-        </View>
-        <View></View>
       </View>
     );
   }
@@ -93,6 +108,36 @@ const styles = StyleSheet.create({
     fontSize: 30,
     marginRight: -160,
     marginTop: 10
-  }
+  },
+  leagueName: {
+    fontFamily: 'sans-serif-thin',
+    // color: 'rgb(255, 197, 66)',
+    color: 'white',
+    position: 'relative',
+    marginRight: 20,
+    marginTop: 10
+  },
+  leagueBox: {
+    marginTop: '6%',
+    marginLeft: '7%',
+    width: '85%',
+    height: '40%',
+    backgroundColor: '#2A3C44',
+    borderRadius: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.00,
+    elevation: 24,
+  },
+  // flag:{
+  //   position:'relative',
+  //   width:100,
+  //   height:100,
+  //   // backgroundColor:'red',
+  // }
 });
 export default Livescore;
