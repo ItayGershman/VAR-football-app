5540bda883b2a34e7ee472260ee2d89d8709a1cb
AvielDagan <48187820+AvielDagan@users.noreply.github.com>
2020-07-18 11:49:07 +0300

    commit

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+const expoPreset = require('jest-expo/jest-preset');
+const jestPreset = require('@testing-library/react-native/jest-preset');
+
+module.exports = Object.assign(expoPreset, jestPreset, {
+    setupFiles: [...expoPreset.setupFiles, ...jestPreset.setupFiles],
+    setupFilesAfterEnv: ['@testing-library/react-native/cleanup-after-each']
+});
