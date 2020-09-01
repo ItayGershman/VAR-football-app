d0f9f6336b533107798a7589c729432c8d778da3
itay <48354712+ItayGershman@users.noreply.github.com>
2020-09-01 14:29:42 +0300

    remove server from front

--- Server_side/consts.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    DB_HOST: process.env.DB_HOST,
-    DB_USER: process.env.DB_USER,
-    DB_PASS: process.env.DB_PASS
-};
--- Server_side/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @project VAR
- * @author Itay Gershman & Aviel Dagan
- * @license Itay Gershman & Aviel Dagan
- */
-
-require('dotenv').config();
-require('./config/db');
-require('./app');
